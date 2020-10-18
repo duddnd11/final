bc05f7ef4aceb16c7fa4242548a8a3d466c3a0ab
ljk80 <ljk80@14.38.136.211>
2020-10-18 19:51:02 +0900

    delete

--- Ex20/src/main/java/com/yw/dao/MemberDao.java
+++
@@ -1,9 +0,0 @@
-package com.yw.dao;
-
-import com.yw.vo.MemberVo;
-
-public interface MemberDao {
-	public void insertMember(MemberVo vo);
-	public MemberVo loginMember(MemberVo vo);
-	public void modifyMember(MemberVo vo);
-}
